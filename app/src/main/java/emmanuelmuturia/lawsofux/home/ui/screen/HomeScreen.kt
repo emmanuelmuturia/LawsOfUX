d87f2223317df7177e21c9e91357043cb74df8e8
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-02-27 12:05:36 +0300

    Set up the skeleton of the Home Screen...

---
+++ app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeScreen.kt
@@ -0,0 +1,29 @@
+package emmanuelmuturia.lawsofux.home.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * This is the app's Home Screen...
+ */
+
+@Composable
+fun HomeScreen() {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.background)
+    ) { paddingValues ->
+        HomeScreenContent(modifier = Modifier.padding(paddingValues = paddingValues))
+    }
+
+}
+
+@Composable
+fun HomeScreenContent(modifier: Modifier) {
+
+}
