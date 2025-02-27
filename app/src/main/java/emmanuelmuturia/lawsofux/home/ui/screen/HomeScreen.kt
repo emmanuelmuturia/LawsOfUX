ecb170e4c081941607e236962936293e1b7938fd
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-02-27 13:13:08 +0300

    Draft the Home Screen's UI elements...

--- app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeScreen.kt
+++ app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeScreen.kt
@@ -1,12 +1,38 @@
 package emmanuelmuturia.lawsofux.home.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import emmanuelmuturia.lawsofux.R
 
 /**
  * This is the app's Home Screen...
@@ -32,12 +58,58 @@ fun HomeScreenContent(modifier: Modifier) {
 
 @Composable
 fun HomeScreenText() {
-
+    Text(
+        text = "Laws of UX is a collection of best practices that designers can consider when building user interfaces.",
+        fontSize = 49.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_mono_regular)))
+    )
 }
 
 @Composable
-fun HomeScreenNotification(modifier: Modifier = Modifier) {
+fun HomeScreenNotification() {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(all = 14.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+    ) {
+        Row {
+            Icon(
+                imageVector = Icons.Rounded.Notifications,
+                contentDescription = "Home Screen Notification Icon",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
 
+            VerticalDivider(
+                modifier = Modifier.fillMaxHeight(),
+                thickness = 3.dp,
+                color = MaterialTheme.colorScheme.background
+            )
+
+            Text(
+                text = buildAnnotatedString {
+                    append(text = "The updated Laws of UX large format index poster is now available! Additions include Paradox of the Active User, Selection Attention, Cognitive Bias, and more. ")
+
+                    withLink(
+                        link = LinkAnnotation.Url(
+                            url = "https://jonyablonski.bigcartel.com/product/laws-of-ux-index-poster",
+                        )
+                    ) {
+                        withStyle(style = SpanStyle(
+                            textDecoration = TextDecoration.Underline
+                        )
+                        ) {
+                            append(text = "Check it out →")
+                        }
+                    }
+                },
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 18.sp,
+                overflow = TextOverflow.Clip,
+                fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular)))
+            )
+        }
+    }
 }
 
 @Composable
@@ -46,6 +118,34 @@ fun HomeScreenCardList(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun HomeScreenCardItem(modifier: Modifier = Modifier) {
+fun HomeScreenCardItem() {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(all = 14.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+    ) {
+        Column {
+            Image(
+                painter = ,
+                contentDescription = "Home Screen Content Thumbnail",
+                contentScale = ContentScale.Crop
+            )
 
+            Text(
+                text = ,
+                fontSize = 25.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                overflow = TextOverflow.Clip,
+                fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_mono_regular))),
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = ,
+                fontSize = 25.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                overflow = TextOverflow.Clip,
+                fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_mono_regular)))
+            )
+        }
+    }
 }
