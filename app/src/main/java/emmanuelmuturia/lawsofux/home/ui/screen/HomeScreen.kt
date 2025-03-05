6c695be37767205229419856c85f8c27b7757d81
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-03-05 12:08:42 +0300

    Add the Home Screen's 'Scroll To Top' functionality...

--- app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeScreen.kt
+++ app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package emmanuelmuturia.lawsofux.home.ui.screen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -12,19 +13,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -42,6 +51,7 @@ import emmanuelmuturia.lawsofux.commons.components.LawsOfUXFooter
 import emmanuelmuturia.lawsofux.commons.components.LawsOfUXTopAppBar
 import emmanuelmuturia.lawsofux.home.data.model.UXLaw
 import emmanuelmuturia.lawsofux.home.ui.state.HomeScreenUIState
+import kotlinx.coroutines.launch
 
 /**
  * This is the app's Home Screen...
@@ -64,8 +74,37 @@ fun HomeScreen() {
 
 @Composable
 private fun HomeScreenContent(modifier: Modifier) {
+
+    val homeScreenListState = rememberLazyListState()
+
+    val showScrollToTopButton by remember {
+        derivedStateOf {
+            homeScreenListState.firstVisibleItemIndex > 0
+        }
+    }
+
+    val homeScreenCoroutineScope = rememberCoroutineScope()
+
+    AnimatedVisibility(visible = showScrollToTopButton) {
+        FloatingActionButton(
+            onClick = {
+                homeScreenCoroutineScope.launch {
+                    homeScreenListState.animateScrollToItem(index = 0)
+                }
+            },
+            containerColor = MaterialTheme.colorScheme.primary,
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.arrow_up),
+                contentDescription = "Scroll to Top Button",
+                tint = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
+    }
+
     LazyColumn(
         modifier = modifier.fillMaxSize(),
+        state = homeScreenListState
     ) {
         item { HomeScreenText() }
         item { HomeScreenNotification() }
