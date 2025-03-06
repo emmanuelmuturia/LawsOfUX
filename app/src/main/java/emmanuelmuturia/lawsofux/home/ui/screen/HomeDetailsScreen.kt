4774955d047806453cf488661f7a167931f10175
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-03-06 16:56:54 +0300

    Set up the skeleton of the Home Details Screen...

--- app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeDetailsScreen.kt
+++ app/src/main/java/emmanuelmuturia/lawsofux/home/ui/screen/HomeDetailsScreen.kt
@@ -1,7 +1,9 @@
 package emmanuelmuturia.lawsofux.home.ui.screen
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -10,17 +12,31 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import emmanuelmuturia.lawsofux.R
+import emmanuelmuturia.lawsofux.home.data.model.UXLaw
 import kotlinx.coroutines.launch
 
 @Composable
@@ -75,33 +91,120 @@ private fun HomeDetailsScreenContent(modifier: Modifier) {
 }
 
 @Composable
-private fun HomeDetailsScreenTitleAndImage(modifier: Modifier = Modifier) {
+private fun HomeDetailsScreenTitleAndImage(
+    uxLaw: UXLaw
+) {
+    // I will refine this later...
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(all = 14.dp),
+            text = uxLaw.uxLawTitle,
+            fontSize = 28.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            overflow = TextOverflow.Clip,
+            fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+            fontWeight = FontWeight.ExtraBold
+        )
 
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = uxLaw.uxLawThumbnail),
+            contentDescription = "Home Details Screen Content Thumbnail",
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
 
 @Composable
-private fun HomeDetailsScreenDefinition(modifier: Modifier = Modifier) {
-
+private fun HomeDetailsScreenDefinition(uxLaw: UXLaw) {
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = uxLaw.uxLawDescription,
+        fontSize = 21.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Composable
 private fun HomeDetailsScreenTakeaways(modifier: Modifier = Modifier) {
-
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = "Takeaways",
+        fontSize = 21.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        fontWeight = FontWeight.ExtraBold
+    )
 }
 
 @Composable
 private fun HomeDetailsScreenOrigins(modifier: Modifier = Modifier) {
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = "Origins",
+        fontSize = 21.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        fontWeight = FontWeight.ExtraBold
+    )
+}
 
+@Composable
+fun HomeDetailsScreenSource(uxLaw: UXLaw) {
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = buildAnnotatedString {
+            uxLaw.uXLawSource?.let {
+                LinkAnnotation.Url(
+                    url = it
+                )
+            }?.let {
+                withLink(
+                    link = it
+                ) {
+                    append(text = "Source")
+                }
+            }
+        },
+        fontSize = 14.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        textDecoration = TextDecoration.Underline
+    )
 }
 
 @Composable
 private fun HomeDetailsScreenFurtherReading(modifier: Modifier = Modifier) {
-
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = "Further Reading",
+        fontSize = 21.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        fontWeight = FontWeight.ExtraBold
+    )
 }
 
 @Composable
 private fun HomeDetailsScreenRelated(modifier: Modifier = Modifier) {
-
+    Text(
+        modifier = Modifier.padding(all = 14.dp),
+        text = "Related",
+        fontSize = 21.sp,
+        color = MaterialTheme.colorScheme.onBackground,
+        overflow = TextOverflow.Clip,
+        fontFamily = FontFamily(fonts = listOf(Font(resId = R.font.ibm_plex_sans_regular))),
+        fontWeight = FontWeight.ExtraBold
+    )
 }
 
 @Composable
