3bed77fd7e2bf8c5de57071f5ad967707ae8094f
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-03-14 10:38:03 +0300

    Set up the Data Source Dependency Injection [:cards]...

--- app/src/main/java/emmanuelmuturia/lawsofux/cards/source/dependencyInjection/CardsSourceKoinModule.kt
+++ app/src/main/java/emmanuelmuturia/lawsofux/cards/source/dependencyInjection/CardsSourceKoinModule.kt
@@ -1,7 +1,15 @@
 package emmanuelmuturia.lawsofux.cards.source.dependencyInjection
 
+import emmanuelmuturia.lawsofux.cards.source.source.LocalCardSource
+import emmanuelmuturia.lawsofux.cards.source.source.LocalCardSourceImplementation
 import org.koin.dsl.module
 
 val cardsSourceKoinModule = module {
+
+    single<LocalCardSource> {
+        LocalCardSourceImplementation(
+            ioDispatcher = get(),
+        )
+    }
 
 }
