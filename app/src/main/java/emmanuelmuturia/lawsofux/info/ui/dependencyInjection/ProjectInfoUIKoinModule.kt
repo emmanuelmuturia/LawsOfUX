e1d762c2834c5aa8ec2f02a4dbc2c577a008e5fa
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-03-14 16:32:29 +0300

    Set up the UI State and ViewModel [:info]...

---
+++ app/src/main/java/emmanuelmuturia/lawsofux/info/ui/dependencyInjection/ProjectInfoUIKoinModule.kt
@@ -0,0 +1,18 @@
+package emmanuelmuturia.lawsofux.info.ui.dependencyInjection
+
+import emmanuelmuturia.lawsofux.info.data.dependencyInjection.projectInfoDataKoinModule
+import emmanuelmuturia.lawsofux.info.ui.viewmodel.ProjectInfoScreenViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val projectInfoUIKoinModule = module {
+
+    viewModel {
+        ProjectInfoScreenViewModel(
+            projectInfoRepository = get(),
+        )
+    }
+
+    includes(module = listOf(projectInfoDataKoinModule))
+
+}
